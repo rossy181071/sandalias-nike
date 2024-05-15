d0981ab8e51d6d13ae1419d622d54fb86e4cd32d
Rossy Lopez <rossy181071@hotmail.com>
2024-05-14 20:57:19 -0600

    Deleted  import litLogo
    Deleted import viteLogo
    
    Added import from lit
    
    Deleted docsHint
    
    Deleted count
    
    Created title
    
    Created paragraph
    
    Created title 1
    
    Created paragraph 1
    
    Added image1Url
    
    Created title 2
    
    Created paragraph 2
    
    Added image 2
    
    Created title 3
    
    Created paragraph 3
    
    Added image 3
    
    Created title 4
    
    Created paragraph 4
    
    Added image 4
    
    Created title 5
    
    Created paragraph 5
    
    Added image 5
    
    Created title 6
    
    Created paragraph 6
    
    Added image 6
    
    Deleted docHint vite and Lite logo
    
    Deleted Count
    
    Created title
    
    Created paragraph
    
    Created title 1
    
    Created paragraph 1
    
    Added image1Url
    
    Created title 2
    
    Created paragraph 2
    
    Added image 2
    
    Created title 3
    
    Created paragraph 3
    
    Added image 3
    
    Created title 4
    
    Created paragraph 4
    
    Added image 4
    
    Created title 5
    
    Created paragraph 5
    
    Added image 5
    
    Created title 6
    
    Created paragraph 6
    
    Added image 6
    
    Deleted div
    
    Deleted classs logo vite logo
    
    Deleted img src litlogo
    
    Deleted div
    
    Deleted class card
    
    Deleted button
    
    Delete count
    
    Delete button
    
    Deleted div
    
    Deleted cass doscHint
    
    Deleted onClick
    
    deleted this count
    
    Created div id
    
    Created h1 this title
    
    Created paragraph1
    
    Created img src image1Url
    
    Created title 2
    
    Created paragraph2
    
    Created image2Url
    
    Created title 3
    
    Created paragraph 3
    
    Created image3Url
    
    Created title 4
    
    Created paragraph 4
    
    Created image4Url
    
    Created title 5
    
    Created paragraph 5
    
    Created image5Url
    
    Created title 6
    
    Created paragraph 6
    
    Created image6Url
    
    Deleted text alingn
    
    Added value body
    
    Deleted logo
    
    Deleted Logo Hover
    
    Delete logo lit hover
    
    Added paragraph color
    
    Added paragraph font family
    
    Added paragraph color
    
    Added title sandalias-nike
    
    Deleted .card
    
    Added sandalias-nike font family
    
    Deleted a{ font weitht
    
    Deleted color
    
    Deleted  tex-decoration
    
    Created sandalias-nike font family
    
    Deleted a hover color
    
    Created sandalias-nike color
    
    Deleted Slotted
    
    Deleted font-size
    
    Deleted line heitht
    
    Created div padding
    
    Deleted button
    
    Deleted cursor pointer
    
    Deleted button hover
    
    Created sandalias-nike background color
    
    Created width
    
    Created font size
    
    Created title 1 color
    
    Created title 2 color
    
    Created title 3 color
    
    Deleted button focus
    
    Deleted button focus visible
    
    Deleted media prefers
    
    Added title 4 color
    
    Added title 5 color
    
    Added title 6 color
    
    Delette button backgroun
    
    Added li style

--- src/my-element.js
+++ src/my-element.js
@@ -1,6 +1,4 @@
-import { LitElement, css, html } from 'lit'
-import litLogo from './assets/lit.svg'
-import viteLogo from '/vite.svg'
+import { LitElement, css, html, nothing } from 'lit'
 
 /**
  * An example element.
@@ -12,45 +10,155 @@ export class MyElement extends LitElement {
   static get properties() {
     return {
       /**
-       * Copy for the read the docs hint.
+       * Este es el titulo de mi pagina
        */
-      docsHint: { type: String },
-
+      title: { type: String },
       /**
-       * The number of times the button has been clicked.
+       * Este es mi parrafo 
        */
-      count: { type: Number },
+      paragraph: { type: String },
+      /**
+       * Este es el titulo 1 de mi pagina
+       */
+      title1: { type: String },
+      /**
+       * Este es el parrafo 1
+       */
+      paragraph1: { type: String },
+      /**
+       * Esta es la url de la imangen 1
+       */
+      image1Url: { type: String },
+      /**
+       * Este es el titulo 2 de mi pagina
+       */
+      title2: { type: String },
+      /**
+       * Este es el parrafo 1
+       */
+      paragraph2: { type: String },
+      /**
+       * Esta es la url de la imangen 2
+       */
+      image2Url: { type: String },
+      /**
+       * Este es el titulo 3 de mi pagina
+       */
+      title3: { type: String },
+      /**
+       * Este es el parrafo 3
+       */
+      paragraph3: { type: String },
+      /**
+       * Esta es la url de la imangen 3
+       */
+      image3Url: { type: String },
+      /**
+       * Este es el titulo 4 de mi pagina
+       */
+      title4: { type: String },
+      /**
+       * Este es el parrafo 4
+       */
+      paragraph4: { type: String },
+      /**
+       * Esta es la url de la imangen 4
+       */
+      image4Url: { type: String },
+      /**
+       * Este es el titulo 4 de mi pagina
+       */
+      title4: { type: String },
+      /**
+       * Este es el parrafo 4
+       */
+      paragraph4: { type: String },
+      /**
+       * Esta es la url de la imangen 4
+       */
+      image4Url: { type: String },
+      /**
+       * Este es el titulo 5 de mi pagina
+       */
+      title5: { type: String },
+      /**
+       * Este es el parrafo 5
+       */
+      paragraph5: { type: String },
+      /**
+       * Esta es la url de la imangen 5
+       */
+      image5Url: { type: String },
+      /**
+       * Este es el titulo 6 de mi pagina
+       */
+      title6: { type: String },
+      /**
+       * Este es el parrafo 6
+       */
+      paragraph6: { type: String },
+      /**
+       * Esta es la url de la imangen 5
+       */
+      image6Url: { type: String },
+      
     }
   }
 
   constructor() {
     super()
-    this.docsHint = 'Click on the Vite and Lit logos to learn more'
-    this.count = 0
+  
+    this.title = 'TENIS'
+    this.paragraph = 'TENIS UNISEX DIFERENTES ACTIVIDADES'
+    this.title1 = 'Bienvenidos Oulet de Tenis'
+    this.paragraph1 = 'Tenis para descanso difereentes colores dama y caballero'
+    this.image1Url = 'https://i.linio.com/p/4514a8bde801329a559c379b9fabae23-product.jpg'
+    this.title2 = 'Tenis con brillitos diferentes colores para dama y ni&a'
+    this.paragraph2 = 'Diferentes tama&os y forms todos con brillitos'
+    this.image2Url = 'https://http2.mlstatic.com/D_NQ_NP_645779-MLM47684717413_092021-O.webp'
+    this.title3 = 'Tenis para practicar Futbol Hombres, ni&os, damas y ni&as'
+    this.paragraph3 = 'Diferentes colores y tallas unisex'
+    this.image3Url = 'https://down-mx.img.susercontent.com/file/cn-11134207-7r98o-ln6null2ail9f9_tn'
+    this.title4 = 'Tenis de moda para Dama y Caballero fosfo'
+    this.paragraph4 = 'Diferentes tono en fosfo para dama y caballero' 
+    this.image4Url = 'https://http2.mlstatic.com/D_NQ_NP_775117-MLM48897543077_012022-O.webp'
+    this.title5 = 'Tenis exclusivo para dama regalo perfecto en una fecha especial'
+    this.paragraph5 = 'Diferentes tallas'
+    this.image5Url = 'https://dpjye2wk9gi5z.cloudfront.net/wcsstore/ExtendedSitesCatalogAssetStore/images/catalog/zoom/1024051-0011V1.jpg'
+    this.title6 = 'Tenis para correr y caminar'
+    this.paragraph6 = 'Tenis para dama y caballero colores unisex confortables'
+    this.image6Url = 'https://media.gq.com.mx/photos/63ee6405f57c61d24bf50732/master/pass/nike-running-shoes-streakfly-invincible.jpg'
   }
+
 
   render() {
     return html`
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
-      </div>
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
-      </div>
-      <p class="read-the-docs">${this.docsHint}</p>
-    `
-  }
+      <div id="TENIS">
+      <h1>${this.title}</h1>
+      <p>${this.paragraph}</p>
+      <h2>${this.title1}</h2>
+      <p>${this.paragraph1}</p>
+      <img src="${this.image1Url}">
+      <h3>${this.title2}</h3>
+      <p>${this.paragraph2}</p>
+      <img src="${this.image2Url}">
+      <h4>${this.title3}</h4>
+      <p>${this.paragraph3}</p>
+      <img src="${this.image3Url}">
+      <h5>${this.title4}</h5>
+      <p>${this.paragraph4}</p>
+      <img src="${this.image4Url}">
+      <h6>${this.title5}</h6>
+      <p>${this.paragraph5}</p>
+      <img src="${this.image5Url}">
+      <h7>${this.title6}</h7>
+      <p>${this.paragraph6}</p>
+      <img src="${this.image6Url}">
 
-  _onClick() {
-    this.count++
+    
+      </div>`
+
+  
   }
 
   static get styles() {
@@ -59,73 +167,66 @@ export class MyElement extends LitElement {
         max-width: 1280px;
         margin: 0 auto;
         padding: 2rem;
-        text-align: center;
+   }
+      body{
+        background-color: plum;
+      }
+      #parrafo {
+        background-color: chartreuse;
+      }
+      #parrafo {
+        font-family: Georgia
+      }
+      #parrafo { 
+        color: rgb(165, 42, 144);
+      }
+      #sandalias-nike {
+        background-color: rgb(187, 109, 109);
+      }
+      #sandalias-nike {
+      font-family: Times;
+      }
+      #sandalias-nike {
+        color: blue;
+      }
+      
+      div{
+    padding: 15vw;
+    }
+    #sandalias-nike {
+      background-color: cadetblue;
+    }
+      img {
+    width:25vw;
+      }
+      p {
+    font-size:10vh;
       }
 
-      .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-        transition: filter 300ms;
-      }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .logo.lit:hover {
-        filter: drop-shadow(0 0 2em #325cffaa);
-      }
-
-      .card {
-        padding: 2em;
-      }
-
-      .read-the-docs {
-        color: #888;
-      }
-
-      a {
-        font-weight: 500;
-        color: #646cff;
-        text-decoration: inherit;
-      }
-      a:hover {
-        color: #535bf2;
-      }
-
-      ::slotted(h1) {
-        font-size: 3.2em;
-        line-height: 1.1;
-      }
-
-      button {
-        border-radius: 8px;
-        border: 1px solid transparent;
-        padding: 0.6em 1.2em;
-        font-size: 1em;
-        font-weight: 500;
-        font-family: inherit;
-        background-color: #1a1a1a;
-        cursor: pointer;
-        transition: border-color 0.25s;
-      }
-      button:hover {
-        border-color: #646cff;
-      }
-      button:focus,
-      button:focus-visible {
-        outline: 4px auto -webkit-focus-ring-color;
-      }
-
-      @media (prefers-color-scheme: light) {
-        a:hover {
-          color: #747bff;
-        }
-        button {
-          background-color: #f9f9f9;
-        }
-      }
+    #titulo-1 {
+      color: coral;
+    }
+    #titulo-2 {
+      color: aqua;
+    }
+    #titulo-3 {
+      color: chartreuse;
+    }
+    #titulo-4 {
+      color: brown;
+    }
+    #titulo-5 {
+      color: darkgreen;
+    }
+    #titulo-6 {
+      color: darkorchid;
+    }
+    li{
+    font-size: 7vw;
+    }
     `
   }
 }
 
-window.customElements.define('my-element', MyElement)
+  
+   window.customElements.define('my-element', MyElement)
